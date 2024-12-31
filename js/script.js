29f26d624161477fe13d7ee379e621d46b65272d
dldkdud0507 <dldkdud0507@naver.com>
2024-12-31 11:03:58 +0900

    header 일단 마무리

--- js/script.js
+++ js/script.js
@@ -23,4 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// 전체메뉴 햄버거 start
 
+console.log(typeof jQuery); // "function"이 출력되어야 함
+
+
+$(document).ready(function () {
+    const menuIcon = $('.menu_icon');
+    const allMenu = $('.all_menu');  // 숨겨진 메뉴 선택
+
+    // 햄버거 메뉴 클릭 이벤트
+    menuIcon.click(function (e) {
+        e.preventDefault();
+
+        console.log("Menu icon clicked"); // 콘솔에 출력되는지 확인
+
+        // 아이콘 애니메이션 활성화
+        menuIcon.toggleClass('active');
+
+        // 메뉴 보이기/숨기기
+        allMenu.slideToggle();  // 부드럽게 나타내거나 사라지게 함
+    });
+});
+// 전체메뉴 햄버거 end
